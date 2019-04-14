85eb0e654e482a6a4e9ff3547249cdee2a99cd15
Tomasz Lewicki <t.w.lewicki@gmail.com>
2019-04-13 22:01:35 -0700

    changed get_dataset.sh

--- get_dataset.sh
+++ get_dataset.sh
@@ -1,1 +1,3 @@
-wget https://github.com/matterport/Mask_RCNN/releases/download/v2.0/mask_rcnn_coco.h5
+wget http://images.cocodataset.org/zips/train2017.zip
+wget http://images.cocodataset.org/zips/val2017.zip
+wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip
